0bbac80387a4cdd29d72aaf524f2d5d8fa626536
prakasitlasree <prakasitlasree@gmail.com>
2016-06-04 01:10:28 +0700

    Initial Framework

--- Employee/LoginForm.Designer.cs
+++ Employee/LoginForm.Designer.cs
@@ -28,11 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.bt_logon = new System.Windows.Forms.Button();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.lblusername = new System.Windows.Forms.Label();
+            this.lblpassword = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblpassword);
+            this.groupBox1.Controls.Add(this.lblusername);
+            this.groupBox1.Controls.Add(this.txtpassword);
+            this.groupBox1.Controls.Add(this.txtusername);
+            this.groupBox1.Controls.Add(this.bt_logon);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(448, 239);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // bt_logon
+            // 
+            this.bt_logon.Location = new System.Drawing.Point(171, 150);
+            this.bt_logon.Name = "bt_logon";
+            this.bt_logon.Size = new System.Drawing.Size(75, 23);
+            this.bt_logon.TabIndex = 0;
+            this.bt_logon.Text = "Logon";
+            this.bt_logon.UseVisualStyleBackColor = true;
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(171, 69);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(148, 20);
+            this.txtusername.TabIndex = 1;
+            this.txtusername.Text = "admin";
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtpassword.Location = new System.Drawing.Point(171, 108);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(148, 20);
+            this.txtpassword.TabIndex = 2;
+            this.txtpassword.Text = "1234";
+            this.txtpassword.UseSystemPasswordChar = true;
+            // 
+            // lblusername
+            // 
+            this.lblusername.AutoSize = true;
+            this.lblusername.Location = new System.Drawing.Point(94, 72);
+            this.lblusername.Name = "lblusername";
+            this.lblusername.Size = new System.Drawing.Size(57, 13);
+            this.lblusername.TabIndex = 3;
+            this.lblusername.Text = "UserName";
+            // 
+            // lblpassword
+            // 
+            this.lblpassword.AutoSize = true;
+            this.lblpassword.Location = new System.Drawing.Point(94, 111);
+            this.lblpassword.Name = "lblpassword";
+            this.lblpassword.Size = new System.Drawing.Size(53, 13);
+            this.lblpassword.TabIndex = 4;
+            this.lblpassword.Text = "Password";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "LoginForm";
+            this.ClientSize = new System.Drawing.Size(472, 270);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "LoginForm";
+            this.Text = "Login Form";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button bt_logon;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.Label lblpassword;
+        private System.Windows.Forms.Label lblusername;
+        private System.Windows.Forms.TextBox txtpassword;
+
     }
 }
